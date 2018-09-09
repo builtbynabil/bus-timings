23978b5ca9418a1b7ca75c3b89d4f7efdc852b24
nabil <nabilrdhwn@gmail.com>
2018-09-09 08:18:38 +0800

    changed some html and made some error handling like showing feedback when user didnt enter a bus stop code

--- website/main.js
+++ website/main.js
@@ -6,52 +6,67 @@ var submitButton = $(".submitButton");
 
 var refreshButton = $(".refreshButton");
 
+var emptyError = $(".emptyError");
+
+emptyError.hide();
+
 submitButton.on("click", function () {
 
-    busStopCodeGet = busStopCodeInput.val()
-    $.get("https://arrivelah.herokuapp.com/?id=" + busStopCodeGet, function (data) {
-
-        busStopCode.append(busStopCodeGet)
-
-        for (var i = 0; i < data["services"].length; i++) {
-
-            var busNumber = data["services"][i]["no"];
-
-            var date = data["services"][i]["next"]["time"].split(/\D/);
-            var dateYear = date[0];
-            var dateMonth = date[1];
-            var dateDay = date[2];
+    if (busStopCodeInput.val() == "") {
+        emptyError.text("ENTER SOMETHING INTO THE BUS STOP CODE")
+        emptyError.show("fast", () => {
+            setInterval(function () {
+                emptyError.hide("fast")
+            }, 5000)
+        });
 
 
-            var time = data["services"][i]["next"]["time"].split(/\D/)
-            var timeHour = time[3];
-            var timeMinute = time[4];
-            var timeSecond = time[5];
+    } else {
+        busStopCodeGet = busStopCodeInput.val()
+        $.get("https://arrivelah.herokuapp.com/?id=" + busStopCodeGet, function (data) {
 
-            var subsequentTime = data["services"][i]["next"]["time"].split(/\D/)
-            var subsequentTimeHour = subsequentTime[3];
-            var subsequentTimeMinute = subsequentTime[4];
-            var subsequentTimeSecond = subsequentTime[5];
+            busStopCode.append(busStopCodeGet)
+
+            for (var i = 0; i < data["services"].length; i++) {
+
+                var busNumber = data["services"][i]["no"];
+
+                var date = data["services"][i]["next"]["time"].split(/\D/);
+                var dateYear = date[0];
+                var dateMonth = date[1];
+                var dateDay = date[2];
 
 
-            var busType;
+                var time = data["services"][i]["next"]["time"].split(/\D/)
+                var timeHour = time[3];
+                var timeMinute = time[4];
+                var timeSecond = time[5];
 
-            if (data["services"][i]["next"]["type"] === "DD") {
-                busType = "Double Deck";
-            } else if (data["services"][i]["next"]["type"] === "SD") {
-                busType = "Single Deck";
+                var subsequentTime = data["services"][i]["subsequent"]["time"].split(/\D/)
+                var subsequentTimeHour = subsequentTime[3];
+                var subsequentTimeMinute = subsequentTime[4];
+                var subsequentTimeSecond = subsequentTime[5];
+
+
+                var busType;
+
+                if (data["services"][i]["next"]["type"] === "DD") {
+                    busType = "Double Deck";
+                } else if (data["services"][i]["next"]["type"] === "SD") {
+                    busType = "Single Deck";
+                }
+
+                busService.append(
+                    "<div class='oneBusService'>" +
+                    "<h1 class='busNumber'>" + busNumber + "</h1>" +
+                    "<p class='busTiming_one'>" + timeHour + ":" + timeMinute + " || " + subsequentTimeHour + ":" + subsequentTimeMinute + "</p>" +
+                    "<p class='busType'>" + busType + "</p>" +
+                    "</div>"
+                )
+
             }
 
-            busService.append(
-                "<div class='oneBusService'>" +
-                "<h1 class='busNumber'>" + busNumber + "</h1>" +
-                "<p class='busTiming_one'>" + timeHour + ":" + timeMinute + " || " + subsequentTimeHour + ":" + subsequentTimeMinute + "</p>" +
-                "<p class='busType'>" + busType + "</p>" +
-                "</div>"
-            )
-
-        }
-
-    })
+        })
+    }
 
 })
